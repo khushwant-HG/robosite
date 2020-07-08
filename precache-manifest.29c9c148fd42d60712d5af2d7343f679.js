39c6566243a22c1c4c322110e62bb3b404327760
khushwant <500062206@stu.upes.ac.in>
2020-07-08 15:14:55 +0530

    Updates

---
+++ precache-manifest.29c9c148fd42d60712d5af2d7343f679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836bb2f6bed5c52ada182ef4e35f1446",
+    "url": "/robosite/index.html"
+  },
+  {
+    "revision": "3b8370c079f7cd1757d6",
+    "url": "/robosite/static/css/main.6ea1239f.chunk.css"
+  },
+  {
+    "revision": "b97a72b924ed79d2e29e",
+    "url": "/robosite/static/js/2.011243ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosite/static/js/2.011243ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8370c079f7cd1757d6",
+    "url": "/robosite/static/js/main.55e7e483.chunk.js"
+  },
+  {
+    "revision": "fafbbd2ddcbcbb6cdedc",
+    "url": "/robosite/static/js/runtime-main.11a7b12f.js"
+  }
+]);
